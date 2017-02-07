eaa85fe271e3e992cbc1902a7a9521654c62635f
FilipTrajkovski <filip_trajkovski94@hotmail.com>
2017-02-07 10:18:39 +0100

    Added switch template

--- Proekt_2017/Proektna_2017.asm
+++ Proekt_2017/Proektna_2017.asm
@@ -44,11 +44,65 @@ L_main2:
 ;Proektna_2017.c,19 :: 		}
 	GOTO       L_main2
 L_main3:
-;Proektna_2017.c,22 :: 		}
+;Proektna_2017.c,20 :: 		switch(kp)
+	GOTO       L_main4
+;Proektna_2017.c,22 :: 		case 1:
+L_main6:
+;Proektna_2017.c,24 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,26 :: 		case 2:
+L_main7:
+;Proektna_2017.c,28 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,30 :: 		case 5:
+L_main8:
+;Proektna_2017.c,32 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,34 :: 		case 6:
+L_main9:
+;Proektna_2017.c,36 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,38 :: 		case 9:
+L_main10:
+;Proektna_2017.c,40 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,42 :: 		case 10:
+L_main11:
+;Proektna_2017.c,44 :: 		break;
+	GOTO       L_main5
+;Proektna_2017.c,46 :: 		}
 L_main4:
-;Proektna_2017.c,23 :: 		}
+	MOVF       _kp+0, 0
+	XORLW      1
+	BTFSC      STATUS+0, 2
+	GOTO       L_main6
+	MOVF       _kp+0, 0
+	XORLW      2
+	BTFSC      STATUS+0, 2
+	GOTO       L_main7
+	MOVF       _kp+0, 0
+	XORLW      5
+	BTFSC      STATUS+0, 2
+	GOTO       L_main8
+	MOVF       _kp+0, 0
+	XORLW      6
+	BTFSC      STATUS+0, 2
+	GOTO       L_main9
+	MOVF       _kp+0, 0
+	XORLW      9
+	BTFSC      STATUS+0, 2
+	GOTO       L_main10
+	MOVF       _kp+0, 0
+	XORLW      10
+	BTFSC      STATUS+0, 2
+	GOTO       L_main11
+L_main5:
+;Proektna_2017.c,47 :: 		oldstate=kp;
+	MOVF       _kp+0, 0
+	MOVWF      _oldstate+0
+;Proektna_2017.c,48 :: 		}
 	GOTO       L_main0
-;Proektna_2017.c,24 :: 		}
+;Proektna_2017.c,49 :: 		}
 L_end_main:
 	GOTO       $+0
 ; end of _main
--- Proekt_2017/Proektna_2017.cp
+++ Proekt_2017/Proektna_2017.cp
@@ -1,6 +1,6 @@
 #line 1 "D:/Users/Public/Documents/Mikroelektronika/mikroC PRO for PIC/Examples/Proektna_2017/Proekt_2017/Proektna_2017.c"
 char keypadPort at PORTD;
-int T1=20,V1=5,T2=25;
+int T1=20,V1=5,T2=25,started=0;
 char kp,oldstate;
 void main()
 {
@@ -18,8 +18,33 @@ void main()
  {
  kp=Keypad_Key_Click();
  }
- switch(kp){
-
+ switch(kp)
+ {
+ case 1:
+ {
+ break;
  }
+ case 2:
+ {
+ break;
+ }
+ case 5:
+ {
+ break;
+ }
+ case 6:
+ {
+ break;
+ }
+ case 9:
+ {
+ break;
+ }
+ case 10:
+ {
+ break;
+ }
+ }
+ oldstate=kp;
  }
 }
